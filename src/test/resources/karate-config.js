beaa5031b82d7b6f14f0b03394d3dee9f8cafd04
JuandresPUCE <jaaviles@puce.edu.ec>
2024-11-11 23:30:43 -0500

    Agregando comentarios en karate-config.js

--- src/test/resources/karate-config.js
+++ src/test/resources/karate-config.js
@@ -1,5 +1,5 @@
 function() {
-    var env = karate.env; // Obtiene el entorno actual desde el sistema o desde Gradle
+    var env = karate.env; // Entorno actual desde el sistema o desde Gradle
     if (!env) {
         env = 'dev'; // Valor predeterminado si no se pasa el entorno
     }
@@ -10,9 +10,9 @@ function() {
     };
 
     if (env === 'qa') {
-        config.urlBase = 'https://petstore.swagger.io/v2';
+        config.urlBase = 'https://petstore.swagger.io/v2'; //apunta a QA para el ejemplo igual que desarrollo
     } else if (env === 'prod') {
-        config.urlBase = 'https://prod-petstore.swagger.io/v3';
+        config.urlBase = 'https://prod-petstore.swagger.io/v3'; // apunta a producción para el ejemplo pero esta debe fallar ya que la url es hipotética
         karate.fail('Pruebas no permitidas en el entorno de producción: ' + env);
     }
 
